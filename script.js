a1b3ab5e570ee5a0ac9a184d387e8511143c911a
KFmota <kfmotatiktok@gmail.com>
2025-06-30 01:24:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('btnMessage');
     button.addEventListener('click', () => {
-        alert('Bravo, tu as cliqué sur le bouton ! 🎉');
+        alert('Severine, 2KM DE CHEZ TOI EST CHAUDE ! 🎉');
     });
 });
